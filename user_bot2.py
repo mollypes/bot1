db044e183d2edbddf1b6ef2c44c0f0366bfea200
mollypes <103337713+mollypes@users.noreply.github.com>
2022-04-18 22:13:19 +0300

    Update user_bot2.py

--- user_bot2.py
+++ user_bot2.py
@@ -14,7 +14,7 @@ import tasks
 
 logging.basicConfig(level=logging.INFO)
 
-API_TOKEN = '2079872666:AAGj-tH_4WasvGQCBvggT9tLzDAlj4OtAy0'
+API_TOKEN = '5214467810:AAFS1TgDD6FESMeRHsymFWqbcMsquz6BROM'
 
 EARN = '💥 Заработать'
 BALANCE = '💰 Баланс'
